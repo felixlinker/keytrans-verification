a03c08217e310c4108c649e65a32015e85f4a453
Linard Arquint <linard.arquint@inf.ethz.ch>
2025-02-07 14:16:26 -0800

    feat: commitment example

---
+++ commitment/commitment.go
@@ -0,0 +1,35 @@
+package commitment
+
+// @ requires low(hash)
+// @ ensures  res ==> low(value)
+func verify(hash int, value int) (res bool) {
+    res = (hash == computeHash(value))
+    return
+}
+
+// @ requires low(hash)
+// @ ensures  res ==> low(value)
+func verifyWithBranching(hash int, value int) (res bool) {
+    // the following if statement fails because the condition is not-low
+    // however, this limitation could be lifted by a more complicated encoding
+    if hash != computeHash(value) {
+        return false
+    }
+    return true
+}
+
+// the following postcondition specifies that the Go function `computeHash` behaves like the
+// pure (mathematical) function `hashFn` for which we assume injectivity (see domain below)
+// @ ensures res == hashFn(input)
+func computeHash(input int) (res int)
+
+/* @
+type HashFunction domain {
+    func hashFn(int) int
+    func invFn(int) int
+
+    axiom { // hashFn is injective
+        forall v int :: { hashFn(v) } invFn(hashFn(v)) == v
+    }
+}
+@ */
