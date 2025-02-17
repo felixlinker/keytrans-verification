51d806344aba9d074465b271a879a4b18c622d38
Cory Francis Myers <cfm@panix.com>
2025-02-17 15:04:02 -0800

    test: reenable verifyWithBranching() under "--hyperMode extended"
    This reverts commit dd58e793225ac5eb9e4a6b9f6662214f72849914.

--- commitment/commitment.go
+++ commitment/commitment.go
@@ -7,7 +7,6 @@ func verify(hash int, value int) (res bool) {
     return
 }
 
-/*
 // @ requires low(hash)
 // @ ensures  res ==> low(value)
 func verifyWithBranching(hash int, value int) (res bool) {
@@ -18,7 +17,6 @@ func verifyWithBranching(hash int, value int) (res bool) {
     }
     return true
 }
-*/
 
 // the following postcondition specifies that the Go function `computeHash` behaves like the
 // pure (mathematical) function `hashFn` for which we assume injectivity (see domain below)
