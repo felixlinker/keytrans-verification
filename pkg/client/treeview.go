779e851ca36eacb6a8ddb24ebcc10a1290ef7548
Felix Linker <linkerfelix@gmail.com>
2025-02-28 13:31:37 +0100

    Implement timestamp update

--- pkg/client/treeview.go
+++ pkg/client/treeview.go
@@ -6,14 +6,87 @@ import (
 	"github.com/felixlinker/keytrans-verification/pkg/proofs"
 )
 
-// This file implements Section 4.2, "Updating Views of the Tree / Algorithm"
+// This file implements Section 4, "Updating Views of the Tree"
+
+type ImplicitBinarySearchTree struct {
+	Root uint64
+	Left *ImplicitBinarySearchTree
+	Right *ImplicitBinarySearchTree
+}
+
+// Get the largest power of two smaller than tree_size
+func RootNode(tree_size uint64) uint64 {
+	var power uint64 = 1
+	for power < tree_size {
+		power = power << 1
+	}
+	return power >> 1
+}
+
+func (tree *ImplicitBinarySearchTree) OffSet(by uint64) {
+	tree.Root += by
+
+	if tree.Left != nil {
+		tree.Left.OffSet(by)
+	}
+	if tree.Right != nil {
+		tree.Right.OffSet(by)
+	}
+}
+
+func (tree *ImplicitBinarySearchTree) PathTo(node uint64) (path []uint64, err error) {
+	if tree == nil {
+		return nil, errors.New("not found")
+	} else if tree.Root == node {
+		return []uint64{ node }, nil
+	} else {
+		var recurse *ImplicitBinarySearchTree
+		if tree.Root < node {
+			recurse = tree.Right
+		} else { // tree.Root > node
+			recurse = tree.Right
+		}
+
+		if path, err := recurse.PathTo(node); err != nil {
+			return nil, err
+		} else {
+			return append([]uint64{ tree.Root }, path...), nil
+		}
+	}
+}
+
+//@ tree != nil ==> len(path) > 0
+func (tree *ImplicitBinarySearchTree) FrontierNodes() (path []uint64) {
+	path = []uint64{}
+	for tree != nil {
+		path = append(path, tree.Root)
+		tree = tree.Right
+	}
+	return path
+}
+
+//@ ensures tree_size != 0 ==> tree != nil
+func MkImplicitBinarySearchTree(tree_size uint64) (tree *ImplicitBinarySearchTree) {
+	root := RootNode(tree_size)
+	if tree_size == 0 {
+		return nil
+	} else if tree_size == 1 {
+		return &ImplicitBinarySearchTree{ root, nil, nil }
+	}
+
+	left := MkImplicitBinarySearchTree(root)
+	right := MkImplicitBinarySearchTree(tree_size - root - 1)
+	right.OffSet(root + 1)
+	return &ImplicitBinarySearchTree{ root, left, right }
+}
 
 type UserState struct {
-	Size                *uint64
+	Size                uint64 // 0 means no tree
 	Full_subtrees       []proofs.NodeValue
 	Frontier_timestamps []uint64
 }
 
+//@ ensures forall i, j int :: i < j && j < len(timestamps) ==> timestamps[i] < timestamps[j]
 func checkIncreasing(timestamps []uint64) bool {
 	if len(timestamps) == 0 {
 		return true
@@ -31,17 +104,30 @@ func checkIncreasing(timestamps []uint64) bool {
 
 //@ ensures e == nil ==> *st.Size == new_head.Tree_head.Tree_size
 func (st *UserState) UpdateView(new_head FullTreeHead, proof proofs.CombinedTreeProof) (err error) {
-	if len(proof.Prefix_proofs) > 0 {
-		return errors.New("invalid proof")
-	} else if len(proof.Prefix_roots) > 0 {
-		return errors.New("invalid proof")
-	} else if !checkIncreasing(proof.Timestamps) {
+	if !checkIncreasing(proof.Timestamps) {
 		return errors.New("timestamps not increasing")
+	} else if (new_head.Tree_head.Tree_size == 0) {
+		return errors.New("new tree cannot be empty")
 	}
 
-	// NOTE: blocked on https://github.com/ietf-wg-keytrans/draft-protocol/issues/16
+	// TODO: Verify proof of consistency
 
-	*(st.Size) = new_head.Tree_head.Tree_size
+	oldSearchTree := MkImplicitBinarySearchTree(st.Size)
+	newSearchTree := MkImplicitBinarySearchTree(new_head.Tree_head.Tree_size)
+	oldFrontier := oldSearchTree.FrontierNodes()
+	newFrontier := newSearchTree.FrontierNodes()
+	if st.Size == 0 {
+		st.Frontier_timestamps = newFrontier
+	} else if pathToOldHead, err := newSearchTree.PathTo(st.Size - 1); err != nil {
+		return err
+	} else {
+		i := 0
+		for pathToOldHead[i] == oldFrontier[i] {
+			i++
+		}
+		st.Frontier_timestamps = append(st.Frontier_timestamps[:i], proof.Timestamps[i:]...)
+	}
 
+	st.Size = new_head.Tree_head.Tree_size
 	return nil
 }
