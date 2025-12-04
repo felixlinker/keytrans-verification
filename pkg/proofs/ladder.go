20fd600a7df7c3f110a89221b0556f15f252027e
AkasakaJelos <kwokwai.lui@hotmail.com>
2025-12-04 10:30:31 +0100

    Push ladder.go

--- pkg/proofs/ladder.go
+++ pkg/proofs/ladder.go
@@ -1,43 +1,155 @@
 package proofs
 
-import (
-	"math"
-)
+//Random pure functions
+
+/*@
+ghost
+requires n > 0
+ensures r >= 0
+ensures n >= 1 ==> IntPow2(r) <= n && IntPow2(r+1) > n && r < n
+decreases n
+pure
+func Log2Floor_pure(n uint64) (r uint64) {
+    return n <= 1 ? 0 : 1 + Log2Floor_pure(n / 2)
+}
+@*/
+
+// Don't think we can do with <= 1 due to correctness, but let's see what will happen lol
+
+/*@
+
+ghost
+requires exp >= 0
+ensures r > 0
+ensures r == (exp == 0 ? 1 : 2 * IntPow2(exp - 1))
+decreases exp
+pure
+func IntPow2(exp uint64) (r uint64) {
+  return exp == 0 ? 1 : 2 * IntPow2(exp - 1)
+}
+
+@*/
+
+/*@
+ghost
+requires t1 > 0
+requires t2 >= t1
+ensures r >= 0
+ensures t1 <= r && r <= t2
+decreases t1
+pure
+func tStar_pure(t1 uint64, t2 uint64, pick_lowest bool) (r uint64) {
+    return let i_low := Log2Floor_pure(t1) in
+           let i_high := Log2Floor_pure(t2) in
+           let low_ := IntPow2(i_low) in
+           i_high > i_low ?
+             (pick_lowest ? IntPow2(i_low + 1) : IntPow2(i_high)) :
+             low_ + tStar_pure(t1 - low_, t2 - low_, false)
+}
+
+@*/
+
+// @ requires base > 0
+// @ ensures r >= 0
+// @ ensures IntPow2(r) <= base
+// @ ensures base < IntPow2(r+1)
+// @ ensures r >= 0
+// @ decreases
+func Log2Floor(base uint64) (r uint64) {
+	var i uint64 = 1
+	var count uint64 = 0
+	//@ invariant i > 0 && count >= 0
+	//@ invariant i == IntPow2(count)
+	//@ invariant count == 0 || IntPow2(count-1) <= base
+	// @ decreases base - i
+	for i <= base {
+		//@ old_i := i
+		i = i * 2
+		//@ old_count := count
+		count = count + 1
+		//@ assert i == old_i *2
+		//@ assert old_count + 1 == count
+	}
+	return count - 1
+}
+
+// @ requires exp >= 0
+// @ ensures r == IntPow2(exp)
+func PowOf2(exp uint64) (r uint64) {
+	var i uint64
+	r = 1
+	//@ invariant i >= 0 && i<= exp
+	//@ invariant r == IntPow2(i)
+	for i = 0; i < exp; i += 1 {
+		r = r * 2
+	}
+	return r
+}
 
 // @ requires t1 >= 0
-// @ requires t2 > t1
+// @ requires t2 >= t1
 func TStar(t1 uint64, t2 uint64) (t_star uint64) {
-	return uint64(tStar(float64(t1+1), float64(t2+1), true) - float64(1))
+	return tStar(t1+1, t2+1, true) - 1
 }
 
 // @ trusted
-func tStar(t1 float64, t2 float64, pick_lowest bool) (t_star float64) {
-	if t1 >= t2 {
-		//@ assert false
-		return -1.0
-	}
-	// @ assert t1 < t2
+func tStar(t1 uint64, t2 uint64, pick_lowest bool) (t_star uint64) {
+	i_low := Log2Floor(t1)
+	i_high := Log2Floor(t2)
+	//@ assert i_low >=0
+	//@ assert i_high>= 0
 
-	i_low := math.Floor(math.Log2(t1))
-	i_high := math.Floor(math.Log2(t2))
-
-	if i_high-i_low > 0.0 {
-		if pick_lowest {
-			return math.Pow(2.0, i_low+1.0)
-		} else {
-			return math.Pow(2.0, i_high)
-		}
+	if i_high-i_low > 0 && pick_lowest {
+		//@ assert i_high > i_low
+		//@ assert pick_lowest
+		return PowOf2(i_low + 1)
+	} else if i_high-i_low > 0 && !pick_lowest {
+		//@ assert i_high > i_low
+		//@ assert !pick_lowest
+		return PowOf2(i_high)
 	} else {
-		low_ := math.Pow(2.0, i_low)
-		// assert int(low_) > 0
-		// assert t1- low_ < t1
-		// assert t2-low_ < t2
+		//@ assert i_high <= i_low
+		low_ := PowOf2(i_low)
+		//@ assert low_ > 0
+		//@ assert t1- low_ < t1
+		//@ assert t2-low_ < t2
 		return low_ + tStar(t1-low_, t2-low_, false)
 	}
 }
 
-// @ preserves acc(r)
+// @ trusted
+func TStar_combined(t1 uint64, t2 uint64, pick_lowest bool, shift_interval bool) (t_star uint64) {
+	if shift_interval {
+		//@ assert shift_interval
+		return TStar_combined(t1+1, t2+1, true, false) - 1
+	} else {
+		//@ assert !shift_interval
+		i_low := Log2Floor(t1)
+		i_high := Log2Floor(t2)
+		//@ assert i_low >=0
+		//@ assert i_high>= 0
+
+		if i_high-i_low > 0 && pick_lowest {
+			//@ assert i_high > i_low
+			//@ assert pick_lowest
+			return PowOf2(i_low + 1)
+		} else if i_high-i_low > 0 && !pick_lowest {
+			//@ assert i_high > i_low
+			//@ assert !pick_lowest
+			return PowOf2(i_high)
+		} else {
+			//@ assert i_high <= i_low
+			low_ := PowOf2(i_low)
+			//@ assert low_ > 0
+			//@ assert t1- low_ < t1
+			//@ assert t2-low_ < t2
+			return low_ + TStar_combined(t1-low_, t2-low_, false, false)
+		}
+	}
+}
+
 // @ requires target >=0
+// @ ensures acc(r)
 //
 //	ensures forall t1 uint32 :: t1 <= target ==> TStar(t1,target) elem r
 //	ensures forall t2 uint32 :: t2 > target ==> TStar(target, t2) elem r
@@ -45,6 +157,7 @@ func FullBinaryLadderSteps(target uint32) (r []uint32) {
 	//@ assume 0 <= target // see https://github.com/viperproject/gobra/issues/192
 	r = make([]uint32, 0)
 	var i uint32 = 1
+	// @ k := 0
 
 	//@ invariant acc(r)
 	//@ invariant 0 <= i - 1
@@ -53,6 +166,7 @@ func FullBinaryLadderSteps(target uint32) (r []uint32) {
 		r = append( /*@ perm(1/2), @*/ r, i-1)
 		//@ old_i := i
 		i = i * 2
+		// @ k = k+1
 		//@ assert i == (2 * old_i)  // Gobra currently does not axiomatize left and right shifts
 	}
 	// i is now the smallest power of two s.t. i-1 is larger than target
@@ -67,8 +181,8 @@ func FullBinaryLadderSteps(target uint32) (r []uint32) {
 	// invariant x_out > target
 	for x_in+1 < x_out {
 		next := x_in + (x_out-x_in)/2
-		//@ assert x_in < next
-		//@ assert next < x_out
+		// assert x_in < next
+		// assert next < x_out
 		r = append( /*@ perm(1/2), @*/ r, next)
 		if next <= target {
 			x_in = next
--- pkg/proofs/ladder_test.go
+++ pkg/proofs/ladder_test.go
@@ -6,9 +6,9 @@ import (
 
 func TestTStar(t *testing.T) {
 	tests := []struct {
-		t1   uint32
-		t2   uint32
-		want uint32
+		t1   uint64
+		t2   uint64
+		want uint64
 	}{
 		{t1: 0, t2: 15, want: 1},
 		{t1: 0, t2: 1, want: 1},
@@ -27,6 +27,35 @@ func TestTStar(t *testing.T) {
 
 	for _, tc := range tests {
 		got := TStar(tc.t1, tc.t2)
+		if got != tc.want {
+			t.Errorf("TStar(%d, %d) = %d; want %d", tc.t1, tc.t2, got, tc.want)
+		}
+	}
+}
+
+func TestTStar_combined(t *testing.T) {
+	tests := []struct {
+		t1   uint64
+		t2   uint64
+		want uint64
+	}{
+		{t1: 0, t2: 15, want: 1},
+		{t1: 0, t2: 1, want: 1},
+		{t1: 8, t2: 9, want: 9},
+		{t1: 8, t2: 14, want: 11},
+		{t1: 9, t2: 10, want: 10},
+		{t1: 8, t2: 16, want: 15},
+		{t1: 7, t2: 16, want: 15},
+		{t1: 1, t2: 2, want: 2},
+		{t1: 2, t2: 3, want: 3},
+		{t1: 15, t2: 16, want: 16},
+		{t1: 16, t2: 17, want: 17},
+		{t1: 0, t2: 17, want: 1},
+		{t1: 0, t2: 1231223121, want: 1},
+	}
+
+	for _, tc := range tests {
+		got := TStar_combined(tc.t1, tc.t2, false, true)
 		if got != tc.want {
 			t.Errorf("TStar(%d, %d) = %d; want %d", tc.t1, tc.t2, got, tc.want)
 		}
