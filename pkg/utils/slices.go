0581a99f12cb09181b8382567008f43f6a3e833a
Felix Linker <linkerfelix@gmail.com>
2026-04-23 23:40:16 +0200

    Add Reverse

--- pkg/utils/slices.go
+++ pkg/utils/slices.go
@@ -31,3 +31,19 @@ func Increment(r_in []uint64, add uint64) (r_out []uint64) {
 	}
 	return r_out
 }
+
+// @ preserves acc(r_in, 1/2)
+// @ ensures acc(r_out)
+// @ ensures len(r_in) == len(r_out)
+// @ ensures forall i int :: 0 <= i && i < len(r_in) ==> r_in[len(r_in)-1-i] == r_out[i]
+func Reverse(r_in []uint64) (r_out []uint64) {
+	r_out = make([]uint64, len(r_in))
+	// @ invariant acc(r_in, 1/2)
+	// @ invariant acc(r_out)
+	// @ invariant 0 <= i && i <= len(r_in) && i <= len(r_out)
+	// @ invariant forall j int :: 0 <= j && j < i ==> r_in[len(r_in)-1-j] == r_out[j]
+	for i := 0; i < len(r_out); i++ {
+		r_out[i] = r_in[len(r_in)-1-i]
+	}
+	return r_out
+}
