8bd9f8ebc995e16c21722d5f6be1177223676357
Felix Linker <linkerfelix@gmail.com>
2026-04-23 23:40:07 +0200

    Add PathToRoot

--- pkg/trees/ibst.go
+++ pkg/trees/ibst.go
@@ -1,7 +1,6 @@
 package trees
 
 import (
-	"github.com/felixlinker/keytrans-verification/pkg/proofs"
 	"github.com/felixlinker/keytrans-verification/pkg/utils"
 )
 
@@ -31,4 +30,31 @@ func frontier(size uint64) (r []uint64) {
 // @ ensures forall i int :: 0 <= i && i < len(r) ==> r[i] < size
 func Frontier(size uint64) (r []uint64) {
 	return utils.Decrement(frontier(size))
+}
+
+// @ requires 0 < n && n <= size
+// @ ensures acc(r)
+// @ ensures 0 < len(r)
+func pathToRoot(n uint64, size uint64) (r []uint64) {
+	i_root := utils.PowOf2(utils.Log2Floor(size))
+	// @ assert i_root <= size
+	var p []uint64
+	if i_root == n {
+		p = []uint64{}
+	} else if i_root < n {
+		p = pathToRoot(n-i_root, i_root-1)
+		p = utils.Increment(p, i_root)
+	} else { // i_root > n
+		// @ assert n <= i_root
+		p = pathToRoot(n, i_root-1)
+	}
+
+	return append( /*@ perm(1/2), @*/ p, i_root)
+}
+
+// @ requires 0 <= n && n < size
+// @ ensures acc(r)
+func PathToRoot(n uint64, size uint64) (r []uint64) {
+	path := pathToRoot(n+1, size)
+	return utils.Decrement(path)
 }
