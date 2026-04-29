e8c2422f2c9627502c3a9a0ff4ce276f4ed05442
Linard Arquint <arquintl@nus.edu.sg>
2026-04-29 20:49:57 +0300

    strengthens some properties of the new implicit binary tree implementation and adapts the 'pathToRoot' implementation

--- pkg/trees/ibst.go
+++ pkg/trees/ibst.go
@@ -4,57 +4,65 @@ import (
 	"github.com/felixlinker/keytrans-verification/pkg/utils"
 )
 
+// ##(--hyperMode extended --enableExperimentalHyperFeatures)
+
 // @ requires 0 < size
-// @ ensures acc(r)
-// @ ensures forall i int :: 0 <= i && i < len(r) ==> 0 < r[i]
-// @ ensures 0 < len(r) && r[len(r) - 1] == size
-// @ ensures forall i int :: 0 <= i && i < len(r) ==> r[i] <= size
+// @ ensures  acc(r)
+// @ ensures  0 < len(r) && uint64(len(r)) <= size
+// @ ensures  forall i int :: { r[i] } 0 <= i && i < len(r) ==> 0 < r[i] && r[i] <= size
+// @ ensures  r[len(r) - 1] == size
+// @ ensures  low(size) ==> low(len(r)) && forall i int :: { r[i] } 0 <= i && i < len(r) ==> low(r[i])
+// @ decreases size
 func frontier(size uint64) (r []uint64) {
 	i_root := utils.PowOf2(utils.Log2Floor(size))
 	// @ assert 0 < i_root && i_root <= size
-	res := []uint64{i_root}
+	r = []uint64{i_root}
 
-	if i_root == size {
-		return res
+	if i_root != size {
+		rec := frontier(size - i_root)
+		r = append( /*@ perm(1/2), @*/ r, utils.Increment(rec, i_root)...)
 	}
 
-	rec := frontier(size - i_root)
-	return append( /*@ perm(1/2), @*/ res, utils.Increment(rec, i_root)...)
+	return
 }
 
 // @ requires 0 < size
-// @ ensures acc(r)
-// @ ensures 0 < len(r)
-// @ ensures forall i int :: 0 <= i && i < len(r) ==> 0 <= r[i]
-// @ ensures r[len(r) - 1] == size - 1
-// @ ensures forall i int :: 0 <= i && i < len(r) ==> r[i] < size
+// @ ensures  acc(r)
+// @ ensures  0 < len(r) && uint64(len(r)) <= size
+// @ ensures  forall i int :: { r[i] } 0 <= i && i < len(r) ==> 0 <= r[i] && r[i] < size
+// @ ensures  r[len(r) - 1] == size - 1
+// @ ensures  low(size) ==> low(len(r)) && forall i int :: { r[i] } 0 <= i && i < len(r) ==> low(r[i])
+// @ decreases
 func Frontier(size uint64) (r []uint64) {
 	return utils.Decrement(frontier(size))
 }
 
-// @ requires 0 < n && n <= size
-// @ ensures acc(r)
-// @ ensures 0 < len(r)
-func pathToRoot(n uint64, size uint64) (r []uint64) {
-	i_root := utils.PowOf2(utils.Log2Floor(size))
-	// @ assert i_root <= size
+// @ requires  min <= n && n <= max
+// @ ensures   acc(r) && 0 < len(r)
+// @ ensures   forall i int :: { r[i] } 0 <= i && i < len(r) ==> min <= r[i] && r[i] <= max
+// @ decreases max - min
+// note that this function operates on tree nodes starting at 0
+// size = 3: nodes 0, 1, 2 with min=0 and max=2
+func pathToRoot(n uint64, min, max uint64) (r []uint64) {
+	i_root := utils.PowOf2(utils.Log2Floor(max+1-min)) + min - 1
+	// @ assert min <= i_root && i_root <= max
 	var p []uint64
 	if i_root == n {
 		p = []uint64{}
 	} else if i_root < n {
-		p = pathToRoot(n-i_root, i_root-1)
-		p = utils.Increment(p, i_root)
-	} else { // i_root > n
-		// @ assert n <= i_root
-		p = pathToRoot(n, i_root-1)
+		p = pathToRoot(n, i_root+1, max)
+	} else {
+		// @ assert n < i_root
+		p = pathToRoot(n, min, i_root-1)
 	}
 
 	return append( /*@ perm(1/2), @*/ p, i_root)
 }
 
 // @ requires 0 <= n && n < size
-// @ ensures acc(r)
+// @ ensures  acc(r) && 0 < len(r)
+// @ ensures  forall i int :: { r[i] } 0 <= i && i < len(r) ==> 0 <= r[i] && r[i] < size
+// @ decreases
 func PathToRoot(n uint64, size uint64) (r []uint64) {
-	path := pathToRoot(n+1, size)
-	return utils.Decrement(path)
+	return pathToRoot(n, 0, size-1)
 }
