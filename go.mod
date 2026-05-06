cebc0ca73c19a66085b6ea2691b5d8b0208ac2d1
Felix Linker <linkerfelix@gmail.com>
2026-05-06 19:10:17 +0200

    Change VRF dependency

--- go.mod
+++ go.mod
@@ -1,8 +1,7 @@
 module github.com/felixlinker/keytrans-verification
 
-go 1.21
+go 1.22
 
-require (
-	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
-	github.com/ProtonMail/go-ecvrf v0.0.1 // indirect
-)
+require github.com/Bren2010/katie v0.0.0-20260501180428-8d949df34857
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- pkg/crypto/vrf.go
+++ pkg/crypto/vrf.go
@@ -3,7 +3,7 @@ package crypto
 import (
 	"bytes"
 
-	"github.com/ProtonMail/go-ecvrf/ecvrf"
+	vrf "github.com/Bren2010/katie/crypto/vrf/edwards25519"
 	"github.com/felixlinker/keytrans-verification/pkg/utils"
 )
 
@@ -22,9 +22,9 @@ func encode(label []byte, version uint64 /*@, ghost p perm @*/) (res []byte) {
 // @ preserves acc(pk, p) && acc(label, p) && acc(prf, p)
 // @ ensures ok ==> len(r) == 32 && acc(r)
 func VRF_verify(pk []byte, label []byte, version uint64, prf []byte /*@, ghost p perm @*/) (r []byte, ok bool) {
-	if pk, err := ecvrf.NewPublicKey(pk /*@, p @*/); err != nil {
+	if pk, err := vrf.NewPublicKey(pk /*@, p @*/); err != nil {
 		return nil, false
-	} else if ok, out, err := pk.Verify(encode(label, version /*@, p @*/), prf /*@, p @*/); !ok || out == nil || err != nil {
+	} else if out, err := pk.Verify(encode(label, version /*@, p @*/), prf /*@, p @*/); out == nil || err != nil {
 		return nil, false
 	} else {
 		// Truncation required in https://www.ietf.org/archive/id/draft-ietf-keytrans-protocol-04.html#name-kt-cipher-suites
