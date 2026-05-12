ee73e8518822cef983ab2c4d1e630343e7555684
Felix Linker <linkerfelix@gmail.com>
2026-05-12 10:01:31 +0200

    Remove unused argument

--- pkg/trees/log.go
+++ pkg/trees/log.go
@@ -209,11 +209,10 @@ func (t *logTree) fillLeftMost(value *[sha256.Size]byte) (ok bool) {
 	}
 }
 
-// @ requires noPerm < p
 // @ requires acc(prf.Inv())
 // @ requires acc(t.Inv()) && unfolding acc(t.Inv()) in 0 < t.size && t.size < newSize
 // @ ensures  acc(newT.Inv()) // && unfolding acc(t.Inv()) in newT.size == newSize
-func (t *logTree) Grow(newSize uint64, prf *proofs.InclusionProof /*@, ghost p perm @*/) (newT *logTree) {
+func (t *logTree) Grow(newSize uint64, prf *proofs.InclusionProof) (newT *logTree) {
 	if /*@ unfolding acc(prf.Inv()) in @*/ prf == nil {
 		panic("non-nil prf")
 	}
