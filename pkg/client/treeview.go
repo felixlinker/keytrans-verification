c68dd6889f42ec002bbfb78b3fdb7e155aebc61d
Linard Arquint <arquintl@nus.edu.sg>
2026-06-04 11:26:56 +0800

    fixes invalid trigger

--- pkg/client/treeview.go
+++ pkg/client/treeview.go
@@ -83,7 +83,7 @@ func (st *UserState) UpdateView(newSize uint64, timestamps []uint64, prf *proofs
 			st.Tree = newTree
 
 			// @ unfold acc(utils.Monotonic(timestamps), p)
-			// @ assert forall i, j int :: {st.Frontier_timestamps[i]} 0 <= i && i < len(st.Frontier_timestamps) && 0 < j && j < len(timestamps) ==> st.Frontier_timestamps[i] < timestamps[j]
+			// @ assert forall i, j int :: {st.Frontier_timestamps[i], timestamps[j]} 0 <= i && i < len(st.Frontier_timestamps) && 0 < j && j < len(timestamps) ==> st.Frontier_timestamps[i] < timestamps[j]
 
 			frontier := search.Frontier(newSize)
 			keep := len(frontier) - len(timestamps)
