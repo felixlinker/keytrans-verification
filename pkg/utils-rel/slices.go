e8b230c3cf7984f31af9f3273f21112d7b476fb2
Felix Linker <linkerfelix@gmail.com>
2026-07-27 22:05:28 +0200

    Prove concat

--- pkg/utils-rel/slices.go
+++ pkg/utils-rel/slices.go
@@ -26,18 +26,25 @@ func Concat(bs1 []byte, bs2 []byte /*@, ghost p perm @*/) (r []byte) {
 	// @ unfold acc(utils.BytesMem(bs2), p)
 	r = append( /*@ p, @*/ r, bs2...)
 	// @ fold acc(utils.BytesMem(bs2), p)
-	// // @ assert len(r) == len(bs1) + len(bs2)
 	if len(r) == 0 {
 		r = []byte{}
 	}
 	// @ assert low(len(bs1)) ==> (low(len(r)) == low(len(bs2)))
-	// @ assert low(len(bs1)) && low(len(r)) ==> (unfolding acc(utils.BytesMem(bs1), p) in forall i int :: {r[i]} {bs1[i]} 0 <= i && i < len(bs1) ==> low(r[i]) == low(bs1[i]))
-	// @ assert low(len(bs1)) && low(len(r)) ==> (unfolding acc(utils.BytesMem(bs2), p) in forall i int :: {r[i]} {bs2[i]} len(bs1) <= i && i < len(r) ==> low(r[i]) == low(bs2[i-len(bs1)]))
-	// @ fold utils.BytesMem(r)
-	// @ assert (low(utils.GetBytesContent(r)) ==> (low(len(r)) && unfolding utils.BytesMem(r) in forall i int :: {r[i]} 0 <= i && i < len(r) ==> low(r[i])))
-	// @ assert ((low(len(r)) && unfolding utils.BytesMem(r) in forall i int :: {r[i]} 0 <= i && i < len(r) ==> low(r[i])) ==> low(utils.GetBytesContent(r)))
-	// @ assert (low(len(bs1)) ==> (low(utils.GetBytesContent(r)) ==> low(len(r)) && low(utils.GetBytesContent(bs1))))
-	// @ assert (low(len(bs1)) ==> (low(utils.GetBytesContent(r)) ==> low(len(r)) && low(utils.GetBytesContent(bs2))))
-	// // @ assert low(len(bs1)) ==> ((low(len(r)) && low(utils.GetBytesContent(r))) == (low(utils.GetBytesContent(bs1)) && low(utils.GetBytesContent(bs2))))
+	// @ unfold acc(utils.BytesMem(bs1), p)
+	// @ unfold acc(utils.BytesMem(bs2), p)
+	// @ assert forall i int :: {r[i]} {bs1[i]} 0 <= i && i < len(bs1) ==> r[i] == bs1[i]
+	// @ assert forall j int :: {r[len(bs1)+j]} {bs2[j]} 0 <= j && j < len(bs2) ==> r[len(bs1)+j] == bs2[j]
+	// @ ghost utils.GetConcatContent(r, bs1, bs2, p)
+	// @ ghost leftContent := utils.GetBytesContent(bs1)
+	// @ ghost rightContent := utils.GetBytesContent(bs2)
+	/*@
+	ghost if low(len(bs1)) {
+		both := leftContent ++ rightContent
+		assert leftContent == both[:len(leftContent)]
+		assert rightContent == both[len(leftContent):]
+		assert low(both) ==> low(both[:len(leftContent)])
+		assert low(both) ==> low(both[len(leftContent):])
+	}
+	@*/
 	return
 }
